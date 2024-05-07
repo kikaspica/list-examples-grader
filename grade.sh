bfab80f3685b2a2e0b6a82e89df07d3bfa25f8ef
Kelly Yang <kikaspicaa@gmail.com>
2024-05-07 09:34:20 -0700

    Added steps 1-4

--- grade.sh
+++ grade.sh
@@ -8,6 +8,26 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+# Step 2 -- Check that the student code contains ListExamples.java
+if [[ -f student-submission/ListExamples.java ]]; then
+    echo "File Existed"
+else
+    echo "ListExamples.java does not exist"
+    echo "Grade: 0"
+    exit
+
+# Step 3 -- Put all the relevant files in the grading-area directory
+cp student-submission/ListExamples.java TestListExamples.java grading-area
+cp -r lib grading-area
+
+# Step 4 -- Compile the java files and check that they compiled successfully
+cd grading-area
+javac -cp $CPATH ListExamples.java TestListExamples.java
+
+echo "This is the exit code of javac (previous command): $?."
+
+# Step 5 -- Run the tests and report the grade based on the JUnit output
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
